371183f1fe35f6a9e5c89f2c4a34fa4e41f1feb5
Victor Marin <marinuribe01@gmail.com>
2023-04-24 12:03:03 -0500

    using .flap and .flapmap

---
+++ server/14-flatmap.js
@@ -0,0 +1,10 @@
+const users = [
+    { userId: 1, username: "Tom", attributes: ["Nice", "Cute"]},
+    { userId: 2, username: "Mike", attributes: ["Lovely"]},
+    { userId: 3, username: "Nico", attributes: ["Nice", "Cool"]},
+  ]
+
+  const rta = users.map(user => user.attributes).flat()
+  const rta2 = users.flatMap(user => user.attributes)
+  console.log(rta);
+  console.log('flatmap',rta2);
